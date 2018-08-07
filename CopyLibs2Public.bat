43ca6f0ae841d2cedc85b4c53ecef8f6b201132d
Aurelien David <aurelien.david@telecom-paristech.fr>
2018-08-07 11:52:13 +0200

    initial commit

---
+++ CopyLibs2Public.bat
@@ -0,0 +1,87 @@
+@echo off
+set OLDDIR=%CD%
+cd /d %~dp0
+
+:begin
+IF "%~1"=="" GOTO MissingParameters
+IF "%~1"=="all" GOTO win32
+IF "%~1"=="x86" GOTO win32
+IF "%~1"=="x64" GOTO win64
+goto MissingParameters
+
+
+:win32
+echo Copying x86 extralibs
+if not exist ..\gpac_public\extra_lib\lib\win32\debug\ mkdir ..\gpac_public\extra_lib\lib\win32\debug\
+if not exist ..\gpac_public\extra_lib\lib\win32\release\ mkdir ..\gpac_public\extra_lib\lib\win32\release\
+if not exist ..\gpac_public\bin\win32\debug\ mkdir ..\gpac_public\bin\win32\debug\
+if not exist ..\gpac_public\bin\win32\release\ mkdir ..\gpac_public\bin\win32\release\
+copy lib\win32\release\*eay* lib\win32\debug\
+copy lib\win32\release\av* lib\win32\debug\
+copy lib\win32\release\swscale* lib\win32\debug\
+copy lib\win32\debug\*.lib ..\gpac_public\extra_lib\lib\win32\debug\
+copy lib\win32\release\*.lib ..\gpac_public\extra_lib\lib\win32\release\
+copy lib\win32\debug\*.dll ..\gpac_public\bin\win32\debug\
+copy lib\win32\release\*.dll ..\gpac_public\bin\win32\release\
+copy lib\win32\release\*.manifest ..\gpac_public\bin\win32\release\
+copy lib\win32\debug\*.plg ..\gpac_public\bin\win32\debug\
+copy lib\win32\release\*.plg ..\gpac_public\bin\win32\release\
+if exist xulrunner-7.0.1.en-US.win32.sdk.zip goto XULSDK_copy
+wget --no-check-certificate https://ftp.mozilla.org/pub/xulrunner/releases/7.0.1/sdk/xulrunner-7.0.1.en-US.win32.sdk.zip
+unzip -n xulrunner-7.0.1.en-US.win32.sdk.zip
+:XULSDK_copy
+xcopy /i /e /q /y xulrunner-sdk ..\gpac_public\extra_lib\include\xulrunner-sdk\
+:DekTec_copy
+unzip -n dektec_dtapi_201505.zip
+if exist SDL-devel-1.2.15-VC.zip goto SDL_copy
+wget http://www.libsdl.org/release/SDL-devel-1.2.15-VC.zip
+unzip SDL-devel-1.2.15-VC.zip
+:SDL_copy
+if not exist ..\gpac_public\extra_lib\include\SDL\ mkdir ..\gpac_public\extra_lib\include\SDL\
+copy SDL-1.2.15\include\ ..\gpac_public\extra_lib\include\SDL\
+copy SDL-1.2.15\lib\x86\*.lib ..\gpac_public\extra_lib\lib\win32\debug\
+copy SDL-1.2.15\lib\x86\*.lib ..\gpac_public\extra_lib\lib\win32\release\
+copy SDL-1.2.15\lib\x86\*.dll ..\gpac_public\bin\win32\debug\
+copy SDL-1.2.15\lib\x86\*.dll ..\gpac_public\bin\win32\release\
+IF "%~1"=="all" GOTO win64
+GOTO done
+
+:win64
+echo Copying x64 extralibs
+if not exist ..\gpac_public\extra_lib\lib\x64\debug\ mkdir ..\gpac_public\extra_lib\lib\x64\debug\
+if not exist ..\gpac_public\extra_lib\lib\x64\release\ mkdir ..\gpac_public\extra_lib\lib\x64\release\
+if not exist ..\gpac_public\bin\x64\debug\ mkdir ..\gpac_public\bin\win32\debug\
+if not exist ..\gpac_public\bin\x64\release\ mkdir ..\gpac_public\bin\x64\release\
+copy lib\x64\release\*eay* lib\x64\debug\
+copy lib\x64\release\av* lib\x64\debug\
+copy lib\x64\release\swscale* lib\x64\debug\
+copy lib\x64\debug\*.lib ..\gpac_public\extra_lib\lib\x64\debug\
+copy lib\x64\release\*.lib ..\gpac_public\extra_lib\lib\x64\release\
+copy lib\x64\debug\*.dll ..\gpac_public\bin\x64\debug\
+copy lib\x64\release\*.dll ..\gpac_public\bin\x64\release\
+copy lib\x64\release\*.manifest ..\gpac_public\bin\x64\release\
+
+if exist xulrunner-7.0.1.en-US.win32.sdk.zip goto XULSDK_copy
+wget --no-check-certificate https://ftp.mozilla.org/pub/xulrunner/releases/7.0.1/sdk/xulrunner-7.0.1.en-US.win32.sdk.zip
+unzip -n xulrunner-7.0.1.en-US.win32.sdk.zip
+:XULSDK_copy
+xcopy /i /e /q /y xulrunner-sdk ..\gpac_public\extra_lib\include\xulrunner-sdk\
+:DekTec_copy
+unzip -n dektec_dtapi_201505.zip
+if exist SDL-devel-1.2.15-VC.zip goto SDL_copy
+wget http://www.libsdl.org/release/SDL-devel-1.2.15-VC.zip
+unzip SDL-devel-1.2.15-VC.zip
+:SDL_copy
+if not exist ..\gpac_public\extra_lib\include\SDL\ mkdir ..\gpac_public\extra_lib\include\SDL\
+copy SDL-1.2.15\include\ ..\gpac_public\extra_lib\include\SDL\
+copy SDL-1.2.15\lib\x64\*.lib ..\gpac_public\extra_lib\lib\x64\debug\
+copy SDL-1.2.15\lib\x64\*.lib ..\gpac_public\extra_lib\lib\x64\release\
+copy SDL-1.2.15\lib\x64\*.dll ..\gpac_public\bin\x64\debug\
+copy SDL-1.2.15\lib\x64\*.dll ..\gpac_public\bin\x64\release\
+GOTO done
+
+:MissingParameters
+Echo You must specified target architecture : either x86, x64 or all
+
+:done
+cd /d %OLDDIR%
---
+++ build_openhevc.bat
@@ -0,0 +1,87 @@
+@echo off
+set OLDDIR=%CD%
+cd /d %~dp0
+
+:begin
+IF "%~2"=="" set MSCV_INIT="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+IF not "%~2"=="" set MSCV_INIT="%~2"
+
+IF "%~3"=="" set BASH_EXE=bash
+IF not "%~3"=="" set BASH_EXE=%~3
+
+IF "%~1"=="" GOTO MissingParameters
+IF "%~1"=="x86" GOTO init32
+IF "%~1"=="x64" GOTO init64
+goto MissingParameters
+
+
+:init32
+set MSCV_ARCH=x86
+set TARGET=win32
+set DEST=..\lib\win32\release\
+goto openhevc
+
+
+:init64
+set MSCV_ARCH=amd64
+set TARGET=win64
+set DEST=..\lib\x64\release\
+goto openhevc
+
+:openhevc
+set GIT=
+for %%e in (%PATHEXT%) do (
+  for %%X in (git%%e) do (
+    if not defined GIT (
+      set GIT=%%~$PATH:X
+    )
+  )
+)
+
+if not defined GIT goto git_not_found
+
+
+IF EXIST ./openHEVC/ goto update_openhevc
+git clone -b ffmpeg_update https://github.com/OpenHEVC/openHEVC.git
+cd openHEVC
+goto build_openhevc
+
+:update_openhevc
+cd openHEVC
+"%GIT%" checkout --force ffmpeg_update
+"%GIT%" fetch
+"%GIT%" reset --hard FETCH_HEAD
+goto build_openhevc
+
+
+:build_openhevc
+call %MSCV_INIT% %MSCV_ARCH%
+
+echo Running make clean...
+"%BASH_EXE%" -c "make clean"
+
+echo Running configure...
+"%BASH_EXE%" ./configure --toolchain=msvc --ld=link.exe  --target-os=%TARGET% --disable-iconv --disable-debug
+
+echo Running make...
+"%BASH_EXE%" -c "make openhevc-shared"
+
+:docopy
+echo Copying libs...
+copy libopenhevc\openhevc.lib %DEST%
+copy libopenhevc\openhevc-1.dll %DEST%
+
+echo All done. Run CopyLibs2Public.bat
+goto done
+
+
+:git_not_found
+echo git not found
+goto done
+
+
+:MissingParameters
+echo "Specify argument x86 or x64. 2nd argument can be path to vcvarsall.bat"
+
+:done
+cd /d %OLDDIR%
